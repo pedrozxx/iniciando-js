df2ef1d0fe30d814925ae506a3c84f0d0194a6f1
pedrozxx <114022656+pedrozxx@users.noreply.github.com>
2024-07-26 12:06:17 -0300

    Add files via upload

---
+++ iniciando com js/main.js
@@ -0,0 +1,52 @@
+//Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+("Hello World!")
+//Crie um script que declare duas variáveis e exiba o resultado da soma entre elas. 
+const numberOne = 40
+const numberTwo = 20
+
+const sum = numberOne + numberTwo
+alert (sum)
+//Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+const isNumber = 29
+if (typeof isNumber =='number'){
+    alert('eh um numero')
+} else {
+    alert ('nao eh um numero')
+}
+//Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+const isString ='pedrao'
+if (typeof isString =='string') {
+    alert('eh uma string')
+} else {
+    alert(' isso nao eh uma string')
+}
+//Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+const isBoolean = true
+if (typeof isBoolean =='boolean'){
+    alert('eh booleano')
+} else {
+    alert('nao eh um booleano' )
+}
+//Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+const sub =numberOne - numberTwo
+alert (sub)
+// Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+const mult = numberOne * numberTwo
+alert (mult)
+//Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+const div = numberOne / numberTwo
+alert (div)
+//Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+const isEvenNumber = 11
+if ( isEvenNumber % 2 ==0){
+    alert ('isso eh par')
+} else {
+    alert('nao eh par')
+}
+//Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+const isOddNumber = 9
+if ( isOddNumber % 2 !==0){
+    alert ('isso eh impar')
+} else {
+    alert('nao eh impar')
+}
